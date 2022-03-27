4b362bfee5d013d9810c29be58faef73d8925858
axel-wheeler-nti-johanneberg <95054908+axel-wheeler-nti-johanneberg@users.noreply.github.com>
2022-03-27 14:20:36 +0200

    revamped entire product and about page aswell as staring optimizing images

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
-window.scrollTo({
-    top: 100,
-    left: 100,
+let header = document.querySelector('#header')
+let aside = document.querySelector('#aside-menu')
+let button = document.querySelector('#menu-button')
+let body = document.querySelector('body')
+let menuOpen = false
+button.onclick = () => {
+  menuOpen = !menuOpen
+  aside.classList.toggle('menu-open')
+  header.classList.toggle('menu-open')
+  body.classList.toggle('menu-open')
+  window.scrollTo({
+    top: 0,
+    left: 0,
     behavior: 'smooth'
   });
+}
+
+// window.scrollTo({
+//     top: 100,
+//     left: 100,
+//     behavior: 'smooth'
+//   });
